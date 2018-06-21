75429362787417c75c41e290e80b73bc20fde47f
p.edwin200294@gmail.com <p.edwin200294@gmail.com>
2018-06-21 09:40:37 +0000

    check 1.0 jboss installer 6.4

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "Registrating EAP as service..."
    EAP_DIR="/home/student/jboss/EAP_6.4/jboss-eap-6.4"
   EAP_SERVICE="/home/student/jboss/EAP_6.4/jboss-eap-6.4/log"
  sudo mkdir $EAP_SERVICE
-
+export JBOSS_HOME = $EAP_DIR
 sudo cp launch.sh /home/student/jboss/EAP_6.4/jboss-eap-6.4/bin/
 sudo cp jboss.service /etc/systemd/system/
 sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
