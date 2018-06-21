0af3c76f5e0a0eb3ed42b999bbdd89d44f4ab064
p.edwin200294@gmail.com <p.edwin200294@gmail.com>
2018-06-21 09:20:55 +0000

    new changes to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-#curl -L -O https://access.redhat.com/cspdownload/62539b312a9759ff899a2bf2d543501e/5b2b4a1b/JBEAP-6.4.0/jboss-eap-6.4.0.zip 
-git clone https://github.com/EdwinPhilip/jboss_zip.git
+curl -L -O https://access.redhat.com/cspdownload/62539b312a9759ff899a2bf2d543501e/5b2b4a1b/JBEAP-6.4.0/jboss-eap-6.4.0.zip 
+#git clone https://github.com/EdwinPhilip/jboss_zip.git
 echo "Installation........................"
 mkdir EAP_6.4
-unzip -d EAP_6.4 -o jboss_zip/jboss-eap-6.4.0.zip
+unzip -d EAP_6.4 -o jboss-eap-6.4.0.zip
 sudo useradd -s /usr/sbin/nologin jboss-EAP
-sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
-sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
+#sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
+#sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
 
 echo "Registrating EAP as service..."
 
@@ -17,6 +17,8 @@ echo "Registrating EAP as service..."
 
 sudo cp launch.sh /home/student/jboss/EAP_6.4/jboss-eap-6.4/bin/
 sudo cp jboss.service /etc/systemd/system/
+sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
+sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
 # sudo cat > /etc/systemd/system/jboss.service << "EOF"
 
 #[UNIT]
