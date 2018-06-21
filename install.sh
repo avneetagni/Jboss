a13003f614805967e9b856b0dbbba2199642ec19
p.edwin200294@gmail.com <p.edwin200294@gmail.com>
2018-06-21 10:57:03 +0000

    check 1.4 jboss 6.4 installer removing zip file after installation

--- install.sh
+++ install.sh
@@ -12,12 +12,13 @@ echo "Registrating EAP as service..."
    EAP_DIR="/home/student/jboss/EAP_6.4/jboss-eap-6.4"
   EAP_SERVICE="/home/student/jboss/EAP_6.4/jboss-eap-6.4/log"
  sudo mkdir $EAP_SERVICE
-export JBOSS_HOME="/home/student/jboss/EAP_6.4/jboss-eap-6.4"
+echo 'JBOSS_HOME="/home/student/jboss/EAP_6.4/jboss-eap-6.4"' >> ~/.bashrc
 
 sudo cp jboss.service /etc/systemd/system/
 sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
 sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
 
+sudo rm -rf jboss-eap-6.4.0.zip
 sudo systemctl stop jboss.service
 sudo systemctl daemon-reload
 sudo systemctl enable jboss.service
