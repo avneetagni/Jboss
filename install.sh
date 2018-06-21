0c2b1b794f19a8866ab333db76d43fa0ed012ebb
p.edwin200294@gmail.com <p.edwin200294@gmail.com>
2018-06-21 10:38:59 +0000

    check 1.3 commit for jboss 6.4 working

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ echo "Installation........................"
 mkdir EAP_6.4
 unzip -d EAP_6.4 -o jboss-eap-6.4.0.zip
 sudo useradd -s /usr/sbin/nologin jboss-EAP
-#sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
-#sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
 
 echo "Registrating EAP as service..."
 
@@ -15,26 +13,12 @@ echo "Registrating EAP as service..."
   EAP_SERVICE="/home/student/jboss/EAP_6.4/jboss-eap-6.4/log"
  sudo mkdir $EAP_SERVICE
 export JBOSS_HOME="/home/student/jboss/EAP_6.4/jboss-eap-6.4"
-sudo cp launch.sh /home/student/jboss/EAP_6.4/jboss-eap-6.4/bin/
+
 sudo cp jboss.service /etc/systemd/system/
 sudo chown -R jboss-EAP:jboss-EAP EAP_6.4
 sudo chown -R jboss-EAP:jboss-EAP EAP_6.4/
-# sudo cat > /etc/systemd/system/jboss.service << "EOF"
 
-#[UNIT]
-#Description = Jboss Agent start
-#After = network.target auditd.service
-
-#[Service]
-#ExeStart=/home/student/jboss/EAP_6.4/jboss-eap-6.4/bin/launch.sh standalone /home/student/jboss/jboss-eap-6.4/standalone/configuration/standalone.xml 0.0.0.0
-#KillMode=process
-#Restart=on-failure
-
-#[Install]    
-#WantedBy=multi-user.target
-
-#EOF
-
+sudo systemctl stop jboss.service
 sudo systemctl daemon-reload
 sudo systemctl enable jboss.service
 
